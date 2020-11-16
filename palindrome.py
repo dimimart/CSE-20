d0ab049f8597e179a39bfb68dc66ecd3d7edf9ea
Diana Martinez <36317627+dimimart@users.noreply.github.com>
2020-11-16 11:18:12 -0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,3 @@
-#part of lab 5
-
 #A palindrome is a word, phrase, or sequence that reads the same backward as forward.
 #Write a code snippet that asks the user to enter a palindrome (“Please, enter a palindrome: ”)
 #and verifies if the word x entered by the user is a palindrome (“The word x is a palindrome.”
